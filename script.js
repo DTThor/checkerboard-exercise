eb3f1e85a227fad306ab6572c0d7b05604932ccd
dtthor <d.t.thorwaldson@gmail.com>
2017-08-22 18:31:26 -0500

    Completed Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,359 @@
-// Your JS goes here
+let blackEl1 = document.createElement('div');
+document.body.appendChild(blackEl1);
+blackEl1.style.width='11.1%';
+blackEl1.style.backgroundColor='black';
+blackEl1.style.position='relative';
+blackEl1.style.float='left';
+blackEl1.style.paddingBottom='11.1%';
+
+let redEl1 = document.createElement('div');
+document.body.appendChild(redEl1);
+redEl1.style.width='11.1%';
+redEl1.style.backgroundColor='red';
+redEl1.style.position='relative';
+redEl1.style.float='left';
+redEl1.style.paddingBottom='11.1%';
+
+let blackEl2 = document.createElement('div');
+document.body.appendChild(blackEl2);
+blackEl2.style.width='11.1%';
+blackEl2.style.backgroundColor='black';
+blackEl2.style.position='relative';
+blackEl2.style.float='left';
+blackEl2.style.paddingBottom='11.1%';
+
+let redEl2 = document.createElement('div');
+document.body.appendChild(redEl2);
+redEl2.style.width='11.1%';
+redEl2.style.backgroundColor='red';
+redEl2.style.position='relative';
+redEl2.style.float='left';
+redEl2.style.paddingBottom='11.1%';
+
+let blackEl3 = document.createElement('div');
+document.body.appendChild(blackEl3);
+blackEl3.style.width='11.1%';
+blackEl3.style.backgroundColor='black';
+blackEl3.style.position='relative';
+blackEl3.style.float='left';
+blackEl3.style.paddingBottom='11.1%';
+
+let redEl3 = document.createElement('div');
+document.body.appendChild(redEl3);
+redEl3.style.width='11.1%';
+redEl3.style.backgroundColor='red';
+redEl3.style.position='relative';
+redEl3.style.float='left';
+redEl3.style.paddingBottom='11.1%';
+
+let blackEl4 = document.createElement('div');
+document.body.appendChild(blackEl4);
+blackEl4.style.width='11.1%';
+blackEl4.style.backgroundColor='black';
+blackEl4.style.position='relative';
+blackEl4.style.float='left';
+blackEl4.style.paddingBottom='11.1%';
+
+let redEl4 = document.createElement('div');
+document.body.appendChild(redEl4);
+redEl4.style.width='11.1%';
+redEl4.style.backgroundColor='red';
+redEl4.style.position='relative';
+redEl4.style.float='left';
+redEl4.style.paddingBottom='11.1%';
+
+let blackEl5 = document.createElement('div');
+document.body.appendChild(blackEl5);
+blackEl5.style.width='11.1%';
+blackEl5.style.backgroundColor='black';
+blackEl5.style.position='relative';
+blackEl5.style.float='left';
+blackEl5.style.paddingBottom='11.1%';
+
+let redEl5 = document.createElement('div');
+document.body.appendChild(redEl5);
+redEl5.style.width='11.1%';
+redEl5.style.backgroundColor='red';
+redEl5.style.position='relative';
+redEl5.style.float='left';
+redEl5.style.paddingBottom='11.1%';
+
+let blackEl6 = document.createElement('div');
+document.body.appendChild(blackEl6);
+blackEl6.style.width='11.1%';
+blackEl6.style.backgroundColor='black';
+blackEl6.style.position='relative';
+blackEl6.style.float='left';
+blackEl6.style.paddingBottom='11.1%';
+
+let redEl6 = document.createElement('div');
+document.body.appendChild(redEl6);
+redEl6.style.width='11.1%';
+redEl6.style.backgroundColor='red';
+redEl6.style.position='relative';
+redEl6.style.float='left';
+redEl6.style.paddingBottom='11.1%';
+
+let blackEl7 = document.createElement('div');
+document.body.appendChild(blackEl7);
+blackEl7.style.width='11.1%';
+blackEl7.style.backgroundColor='black';
+blackEl7.style.position='relative';
+blackEl7.style.float='left';
+blackEl7.style.paddingBottom='11.1%';
+
+let redEl7 = document.createElement('div');
+document.body.appendChild(redEl7);
+redEl7.style.width='11.1%';
+redEl7.style.backgroundColor='red';
+redEl7.style.position='relative';
+redEl7.style.float='left';
+redEl7.style.paddingBottom='11.1%';
+
+let blackEl8 = document.createElement('div');
+document.body.appendChild(blackEl8);
+blackEl8.style.width='11.1%';
+blackEl8.style.backgroundColor='black';
+blackEl8.style.position='relative';
+blackEl8.style.float='left';
+blackEl8.style.paddingBottom='11.1%';
+
+let redEl8 = document.createElement('div');
+document.body.appendChild(redEl8);
+redEl8.style.width='11.1%';
+redEl8.style.backgroundColor='red';
+redEl8.style.position='relative';
+redEl8.style.float='left';
+redEl8.style.paddingBottom='11.1%';
+
+let blackEl9 = document.createElement('div');
+document.body.appendChild(blackEl9);
+blackEl9.style.width='11.1%';
+blackEl9.style.backgroundColor='black';
+blackEl9.style.position='relative';
+blackEl9.style.float='left';
+blackEl9.style.paddingBottom='11.1%';
+
+let redEl9 = document.createElement('div');
+document.body.appendChild(redEl9);
+redEl9.style.width='11.1%';
+redEl9.style.backgroundColor='red';
+redEl9.style.position='relative';
+redEl9.style.float='left';
+redEl9.style.paddingBottom='11.1%';
+
+let blackEl10 = document.createElement('div');
+document.body.appendChild(blackEl10);
+blackEl10.style.width='11.1%';
+blackEl10.style.backgroundColor='black';
+blackEl10.style.position='relative';
+blackEl10.style.float='left';
+blackEl10.style.paddingBottom='11.1%';
+
+let redEl10 = document.createElement('div');
+document.body.appendChild(redEl10);
+redEl10.style.width='11.1%';
+redEl10.style.backgroundColor='red';
+redEl10.style.position='relative';
+redEl10.style.float='left';
+redEl10.style.paddingBottom='11.1%';
+
+let blackEl11 = document.createElement('div');
+document.body.appendChild(blackEl11);
+blackEl11.style.width='11.1%';
+blackEl11.style.backgroundColor='black';
+blackEl11.style.position='relative';
+blackEl11.style.float='left';
+blackEl11.style.paddingBottom='11.1%';
+
+let redEl11 = document.createElement('div');
+document.body.appendChild(redEl11);
+redEl11.style.width='11.1%';
+redEl11.style.backgroundColor='red';
+redEl11.style.position='relative';
+redEl11.style.float='left';
+redEl11.style.paddingBottom='11.1%';
+
+let blackEl12 = document.createElement('div');
+document.body.appendChild(blackEl12);
+blackEl12.style.width='11.1%';
+blackEl12.style.backgroundColor='black';
+blackEl12.style.position='relative';
+blackEl12.style.float='left';
+blackEl12.style.paddingBottom='11.1%';
+
+let redEl12 = document.createElement('div');
+document.body.appendChild(redEl12);
+redEl12.style.width='11.1%';
+redEl12.style.backgroundColor='red';
+redEl12.style.position='relative';
+redEl12.style.float='left';
+redEl12.style.paddingBottom='11.1%';
+
+let blackEl13 = document.createElement('div');
+document.body.appendChild(blackEl13);
+blackEl13.style.width='11.1%';
+blackEl13.style.backgroundColor='black';
+blackEl13.style.position='relative';
+blackEl13.style.float='left';
+blackEl13.style.paddingBottom='11.1%';
+
+let redEl13 = document.createElement('div');
+document.body.appendChild(redEl13);
+redEl13.style.width='11.1%';
+redEl13.style.backgroundColor='red';
+redEl13.style.position='relative';
+redEl13.style.float='left';
+redEl13.style.paddingBottom='11.1%';
+
+let blackEl14 = document.createElement('div');
+document.body.appendChild(blackEl14);
+blackEl14.style.width='11.1%';
+blackEl14.style.backgroundColor='black';
+blackEl14.style.position='relative';
+blackEl14.style.float='left';
+blackEl14.style.paddingBottom='11.1%';
+
+let redEl14 = document.createElement('div');
+document.body.appendChild(redEl14);
+redEl14.style.width='11.1%';
+redEl14.style.backgroundColor='red';
+redEl14.style.position='relative';
+redEl14.style.float='left';
+redEl14.style.paddingBottom='11.1%';
+
+let blackEl15 = document.createElement('div');
+document.body.appendChild(blackEl15);
+blackEl15.style.width='11.1%';
+blackEl15.style.backgroundColor='black';
+blackEl15.style.position='relative';
+blackEl15.style.float='left';
+blackEl15.style.paddingBottom='11.1%';
+
+let redEl15 = document.createElement('div');
+document.body.appendChild(redEl15);
+redEl15.style.width='11.1%';
+redEl15.style.backgroundColor='red';
+redEl15.style.position='relative';
+redEl15.style.float='left';
+redEl15.style.paddingBottom='11.1%';
+
+let blackEl16 = document.createElement('div');
+document.body.appendChild(blackEl16);
+blackEl16.style.width='11.1%';
+blackEl16.style.backgroundColor='black';
+blackEl16.style.position='relative';
+blackEl16.style.float='left';
+blackEl16.style.paddingBottom='11.1%';
+
+let redEl16 = document.createElement('div');
+document.body.appendChild(redEl16);
+redEl16.style.width='11.1%';
+redEl16.style.backgroundColor='red';
+redEl16.style.position='relative';
+redEl16.style.float='left';
+redEl16.style.paddingBottom='11.1%';
+
+let blackEl17 = document.createElement('div');
+document.body.appendChild(blackEl17);
+blackEl17.style.width='11.1%';
+blackEl17.style.backgroundColor='black';
+blackEl17.style.position='relative';
+blackEl17.style.float='left';
+blackEl17.style.paddingBottom='11.1%';
+
+let redEl17 = document.createElement('div');
+document.body.appendChild(redEl17);
+redEl17.style.width='11.1%';
+redEl17.style.backgroundColor='red';
+redEl17.style.position='relative';
+redEl17.style.float='left';
+redEl17.style.paddingBottom='11.1%';
+
+let blackEl18 = document.createElement('div');
+document.body.appendChild(blackEl18);
+blackEl18.style.width='11.1%';
+blackEl18.style.backgroundColor='black';
+blackEl18.style.position='relative';
+blackEl18.style.float='left';
+blackEl18.style.paddingBottom='11.1%';
+
+let redEl18 = document.createElement('div');
+document.body.appendChild(redEl18);
+redEl18.style.width='11.1%';
+redEl18.style.backgroundColor='red';
+redEl18.style.position='relative';
+redEl18.style.float='left';
+redEl18.style.paddingBottom='11.1%';
+
+let blackEl19 = document.createElement('div');
+document.body.appendChild(blackEl19);
+blackEl19.style.width='11.1%';
+blackEl19.style.backgroundColor='black';
+blackEl19.style.position='relative';
+blackEl19.style.float='left';
+blackEl19.style.paddingBottom='11.1%';
+
+let redEl19 = document.createElement('div');
+document.body.appendChild(redEl19);
+redEl19.style.width='11.1%';
+redEl19.style.backgroundColor='red';
+redEl19.style.position='relative';
+redEl19.style.float='left';
+redEl19.style.paddingBottom='11.1%';
+
+let blackEl20 = document.createElement('div');
+document.body.appendChild(blackEl20);
+blackEl20.style.width='11.1%';
+blackEl20.style.backgroundColor='black';
+blackEl20.style.position='relative';
+blackEl20.style.float='left';
+blackEl20.style.paddingBottom='11.1%';
+
+let redEl20 = document.createElement('div');
+document.body.appendChild(redEl20);
+redEl20.style.width='11.1%';
+redEl20.style.backgroundColor='red';
+redEl20.style.position='relative';
+redEl20.style.float='left';
+redEl20.style.paddingBottom='11.1%';
+
+let blackEl21 = document.createElement('div');
+document.body.appendChild(blackEl21);
+blackEl21.style.width='11.1%';
+blackEl21.style.backgroundColor='black';
+blackEl21.style.position='relative';
+blackEl21.style.float='left';
+blackEl21.style.paddingBottom='11.1%';
+
+let redEl21 = document.createElement('div');
+document.body.appendChild(redEl21);
+redEl21.style.width='11.1%';
+redEl21.style.backgroundColor='red';
+redEl21.style.position='relative';
+redEl21.style.float='left';
+redEl21.style.paddingBottom='11.1%';
+
+let blackEl22 = document.createElement('div');
+document.body.appendChild(blackEl22);
+blackEl22.style.width='11.1%';
+blackEl22.style.backgroundColor='black';
+blackEl22.style.position='relative';
+blackEl22.style.float='left';
+blackEl22.style.paddingBottom='11.1%';
+
+let redEl22 = document.createElement('div');
+document.body.appendChild(redEl22);
+redEl22.style.width='11.1%';
+redEl22.style.backgroundColor='red';
+redEl22.style.position='relative';
+redEl22.style.float='left';
+redEl22.style.paddingBottom='11.1%';
+
+let blackEl23 = document.createElement('div');
+document.body.appendChild(blackEl23);
+blackEl23.style.width='11.1%';
+blackEl23.style.backgroundColor='black';
+blackEl23.style.position='relative';
+blackEl23.style.float='left';
+blackEl23.style.paddingBottom='11.1%';
